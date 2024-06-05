d1bce05818c8763bc88ad07b946a145397a518e3
Varsha Saindane <varshasaindane8640@gmail.com>
2024-06-05 17:36:35 +0530

    Added TIC-TAC-TOE Game.cpp

---
+++ TIC-TAC-TOE Game.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to display the game board
+void displayBoard(vector<vector<char>> board) {
+    cout << "  1 2 3\n";
+    for (int i = 0; i < 3; i++) {
+        cout << i + 1 << " ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Function to check if a player has won
+bool checkWin(vector<vector<char>> board, char player) {
+    // Check rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            return true;
+        }
+    }
+
+    // Check columns
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
+            return true;
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] == player && board[1][1] == player && board[2][2]
